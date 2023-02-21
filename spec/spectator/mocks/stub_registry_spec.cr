0c8a5731a3174166bcb661f93feb8f534bc37485
Michael Miller <icy.arctic.fox@gmail.com>
2023-02-20 21:04:25 -0700

    Implement StubRegistry and test

---
+++ spec/spectator/mocks/stub_registry_spec.cr
@@ -0,0 +1,564 @@
+require "../../spec_helper"
+
+def create_registry
+  Spectator::Mocks::StubRegistry.new
+end
+
+def create_example_stub(args = nil)
+  Spectator::Mocks::NilStub.new(:example, args)
+end
+
+def create_example_call(args = Spectator::Mocks::Arguments.none)
+  Spectator::Mocks::Call.new(:example, args)
+end
+
+def sample_args
+  Spectator::Mocks::Arguments.new({arg: 42}, nil, nil, NamedTuple.new)
+end
+
+record ExampleRecord1, value : Int32
+
+record ExampleRecord2, value : Int32
+
+describe Spectator::Mocks::StubRegistry do
+  context "with reference types" do
+    it "stores a stub for an object" do
+      registry = create_registry
+      object = %w[foo bar]
+
+      stub = create_example_stub
+      call = create_example_call
+
+      registry.add_stub(object, stub)
+      registry.find_stub(object, call).should be(stub)
+    end
+
+    describe "#find_stub" do
+      it "returns nil when there are no stubs" do
+        registry = create_registry
+        object = %w[foo bar]
+        call = create_example_call
+
+        registry.find_stub(object, call).should be_nil
+      end
+
+      it "returns when no stubs match" do
+        registry = create_registry
+        object = %w[foo bar]
+
+        stub = Spectator::Mocks::NilStub.new(:foo)
+        call = Spectator::Mocks::Call.new(:bar)
+
+        registry.add_stub(object, stub)
+        registry.find_stub(object, call).should be_nil
+      end
+
+      it "returns a stub matching the method name" do
+        registry = create_registry
+        object = %w[foo bar]
+
+        stub1 = Spectator::Mocks::NilStub.new(:foo)
+        stub2 = Spectator::Mocks::NilStub.new(:bar)
+        call = Spectator::Mocks::Call.new(:foo)
+
+        registry.add_stub(object, stub1)
+        registry.add_stub(object, stub2)
+        registry.find_stub(object, call).should be(stub1)
+      end
+
+      it "returns a stub matching arguments" do
+        registry = create_registry
+        object = %w[foo bar]
+
+        stub1 = Spectator::Mocks::NilStub.new(:foo, Spectator::Mocks::ArgumentsPattern.build(Int32))
+        stub2 = Spectator::Mocks::NilStub.new(:foo, Spectator::Mocks::ArgumentsPattern.build(String))
+        call = Spectator::Mocks::Call.new(:foo, sample_args)
+
+        registry.add_stub(object, stub1)
+        registry.add_stub(object, stub2)
+        registry.find_stub(object, call).should be(stub1)
+      end
+
+      it "doesn't match stubs for other objects" do
+        registry = create_registry
+        object1 = %w[foo bar]
+        object2 = "foobar"
+
+        stub1 = create_example_stub
+        stub2 = create_example_stub
+        call = create_example_call
+
+        registry.add_stub(object1, stub1)
+        registry.add_stub(object2, stub2)
+        registry.find_stub(object1, call).should be(stub1)
+      end
+    end
+
+    describe "#clear_stubs" do
+      it "removes a previously added stub" do
+        registry = create_registry
+        object = %w[foo bar]
+
+        stub = create_example_stub
+        call = create_example_call
+
+        registry.add_stub(object, stub)
+        registry.clear_stubs(object)
+        registry.find_stub(object, call).should be_nil
+      end
+
+      it "doesn't modify other object stubs" do
+        registry = create_registry
+        object1 = %w[foo bar]
+        object2 = "foobar"
+
+        stub1 = create_example_stub
+        stub2 = create_example_stub
+        call = create_example_call
+
+        registry.add_stub(object1, stub1)
+        registry.add_stub(object2, stub2)
+        registry.clear_stubs(object1)
+        registry.find_stub(object2, call).should be(stub2)
+      end
+    end
+
+    it "stores a call for an object" do
+      registry = create_registry
+      object = %w[foo bar]
+      call = create_example_call
+
+      registry.add_call(object, call)
+      registry.calls(object).should contain(call)
+    end
+
+    describe "#calls" do
+      it "returns an empty list for an unknown object" do
+        registry = create_registry
+        object = %w[foo bar]
+
+        registry.calls(object).empty?.should be_true
+      end
+
+      it "returns calls only for an object" do
+        registry = create_registry
+        object1 = %w[foo bar]
+        object2 = "foobar"
+
+        call1 = create_example_call
+        call2 = create_example_call
+
+        registry.add_call(object1, call1)
+        registry.add_call(object2, call2)
+        registry.calls(object1).should_not contain(call2)
+      end
+    end
+
+    describe "#clear_calls" do
+      it "removes a previously added call" do
+        registry = create_registry
+        object = %w[foo bar]
+        call = create_example_call
+
+        registry.add_call(object, call)
+        registry.clear_calls(object)
+        registry.calls(object).empty?.should be_true
+      end
+
+      it "doesn't modify other object calls" do
+        registry = create_registry
+        object1 = %w[foo bar]
+        object2 = "foobar"
+
+        call1 = create_example_call
+        call2 = create_example_call
+
+        registry.add_call(object1, call1)
+        registry.add_call(object2, call2)
+        registry.clear_calls(object1)
+        registry.calls(object2).should contain(call2)
+      end
+    end
+
+    describe "#clear" do
+      it "removes a previously added stub" do
+        registry = create_registry
+        object = %w[foo bar]
+
+        stub = create_example_stub
+        call = create_example_call
+
+        registry.add_stub(object, stub)
+        registry.clear
+        registry.find_stub(object, call).should be_nil
+      end
+
+      it "removes a previously added call" do
+        registry = create_registry
+        object = %w[foo bar]
+        call = create_example_call
+
+        registry.add_call(object, call)
+        registry.clear
+        registry.calls(object).empty?.should be_true
+      end
+
+      context "with an object" do
+        it "removes a previously added stub" do
+          registry = create_registry
+          object = %w[foo bar]
+
+          stub = create_example_stub
+          call = create_example_call
+
+          registry.add_stub(object, stub)
+          registry.clear(object)
+          registry.find_stub(object, call).should be_nil
+        end
+
+        it "removes a previously added call" do
+          registry = create_registry
+          object = %w[foo bar]
+          call = create_example_call
+
+          registry.add_call(object, call)
+          registry.clear(object)
+          registry.calls(object).empty?.should be_true
+        end
+
+        it "doesn't modify other object stubs" do
+          registry = create_registry
+          object1 = %w[foo bar]
+          object2 = "foobar"
+
+          stub1 = create_example_stub
+          stub2 = create_example_stub
+          call = create_example_call
+
+          registry.add_stub(object1, stub1)
+          registry.add_stub(object2, stub2)
+          registry.clear(object1)
+          registry.find_stub(object2, call).should be(stub2)
+        end
+
+        it "doesn't modify other object calls" do
+          registry = create_registry
+          object1 = %w[foo bar]
+          object2 = "foobar"
+
+          call1 = create_example_call
+          call2 = create_example_call
+
+          registry.add_call(object1, call1)
+          registry.add_call(object2, call2)
+          registry.clear(object1)
+          registry.calls(object2).should contain(call2)
+        end
+      end
+    end
+  end
+
+  context "with value types" do
+    it "stores a stub for an object" do
+      registry = create_registry
+      object = 42
+
+      stub = create_example_stub
+      call = create_example_call
+
+      registry.add_stub(object, stub)
+      registry.find_stub(object, call).should be(stub)
+    end
+
+    describe "#find_stub" do
+      it "returns nil when there are no stubs" do
+        registry = create_registry
+        object = 42
+        call = create_example_call
+
+        registry.find_stub(object, call).should be_nil
+      end
+
+      it "returns when no stubs match" do
+        registry = create_registry
+        object = 42
+
+        stub = Spectator::Mocks::NilStub.new(:foo)
+        call = Spectator::Mocks::Call.new(:bar)
+
+        registry.add_stub(object, stub)
+        registry.find_stub(object, call).should be_nil
+      end
+
+      it "returns a stub matching the method name" do
+        registry = create_registry
+        object = 42
+
+        stub1 = Spectator::Mocks::NilStub.new(:foo)
+        stub2 = Spectator::Mocks::NilStub.new(:bar)
+        call = Spectator::Mocks::Call.new(:foo)
+
+        registry.add_stub(object, stub1)
+        registry.add_stub(object, stub2)
+        registry.find_stub(object, call).should be(stub1)
+      end
+
+      it "returns a stub matching arguments" do
+        registry = create_registry
+        object = 42
+
+        stub1 = Spectator::Mocks::NilStub.new(:foo, Spectator::Mocks::ArgumentsPattern.build(Int32))
+        stub2 = Spectator::Mocks::NilStub.new(:foo, Spectator::Mocks::ArgumentsPattern.build(String))
+        call = Spectator::Mocks::Call.new(:foo, sample_args)
+
+        registry.add_stub(object, stub1)
+        registry.add_stub(object, stub2)
+        registry.find_stub(object, call).should be(stub1)
+      end
+
+      it "doesn't confuse two objects with differing types and the same bytes" do
+        registry = create_registry
+        object1 = ExampleRecord1.new(42)
+        object2 = ExampleRecord2.new(42)
+
+        stub1 = create_example_stub
+        stub2 = create_example_stub
+        call = create_example_call
+
+        registry.add_stub(object1, stub1)
+        registry.add_stub(object2, stub2)
+        registry.find_stub(object1, call).should be(stub1)
+      end
+    end
+
+    describe "#clear_stubs" do
+      it "removes a previously added stub" do
+        registry = create_registry
+        object = 42
+
+        stub = create_example_stub
+        call = create_example_call
+
+        registry.add_stub(object, stub)
+        registry.clear_stubs(object)
+        registry.find_stub(object, call).should be_nil
+      end
+
+      it "doesn't modify other object stubs" do
+        registry = create_registry
+        object1 = 42
+        object2 = :xyz
+
+        stub1 = create_example_stub
+        stub2 = create_example_stub
+        call = create_example_call
+
+        registry.add_stub(object1, stub1)
+        registry.add_stub(object2, stub2)
+        registry.clear_stubs(object1)
+        registry.find_stub(object2, call).should be(stub2)
+      end
+
+      it "doesn't confuse two objects with differing types and the same bytes" do
+        registry = create_registry
+        object1 = ExampleRecord1.new(42)
+        object2 = ExampleRecord2.new(42)
+
+        stub1 = create_example_stub
+        stub2 = create_example_stub
+        call = create_example_call
+
+        registry.add_stub(object1, stub1)
+        registry.add_stub(object2, stub2)
+        registry.clear_stubs(object1)
+        registry.find_stub(object1, call).should be_nil
+        registry.find_stub(object2, call).should be(stub1)
+      end
+    end
+
+    it "stores a call for an object" do
+      registry = create_registry
+      object = 42
+      call = create_example_call
+
+      registry.add_call(object, call)
+      registry.calls(object).should contain(call)
+    end
+
+    describe "#calls" do
+      it "returns an empty list for an unknown object" do
+        registry = create_registry
+        object = 42
+
+        registry.calls(object).empty?.should be_true
+      end
+
+      it "returns calls only for an object" do
+        registry = create_registry
+        object1 = 42
+        object2 = :xyz
+
+        call1 = create_example_call
+        call2 = create_example_call
+
+        registry.add_call(object1, call1)
+        registry.add_call(object2, call2)
+        registry.calls(object1).should_not contain(call2)
+      end
+
+      it "doesn't confuse two objects with differing types and the same bytes" do
+        registry = create_registry
+        object1 = ExampleRecord1.new(42)
+        object2 = ExampleRecord2.new(42)
+
+        call1 = create_example_call
+        call2 = create_example_call
+
+        registry.add_call(object1, call1)
+        registry.add_call(object2, call2)
+        registry.calls(object1).should contain(call1)
+        registry.calls(object1).should_not contain(call2)
+      end
+    end
+
+    describe "#clear_calls" do
+      it "removes a previously added call" do
+        registry = create_registry
+        object = 42
+        call = create_example_call
+
+        registry.add_call(object, call)
+        registry.clear_calls(object)
+        registry.calls(object).empty?.should be_true
+      end
+
+      it "doesn't modify other object calls" do
+        registry = create_registry
+        object1 = 42
+        object2 = :xyz
+
+        call1 = create_example_call
+        call2 = create_example_call
+
+        registry.add_call(object1, call1)
+        registry.add_call(object2, call2)
+        registry.clear_calls(object1)
+        registry.calls(object2).should contain(call2)
+      end
+
+      it "doesn't confuse two objects with differing types and the same bytes" do
+        registry = create_registry
+        object1 = ExampleRecord1.new(42)
+        object2 = ExampleRecord2.new(42)
+
+        call1 = create_example_call
+        call2 = create_example_call
+
+        registry.add_call(object1, call1)
+        registry.add_call(object2, call2)
+        registry.clear_calls(object1)
+        registry.calls(object1).empty?.should be_true
+        registry.calls(object2).should contain(call2)
+      end
+    end
+
+    describe "#clear" do
+      it "removes a previously added stub" do
+        registry = create_registry
+        object = 42
+
+        stub = create_example_stub
+        call = create_example_call
+
+        registry.add_stub(object, stub)
+        registry.clear
+        registry.find_stub(object, call).should be_nil
+      end
+
+      it "removes a previously added call" do
+        registry = create_registry
+        object = 42
+        call = create_example_call
+
+        registry.add_call(object, call)
+        registry.clear
+        registry.calls(object).empty?.should be_true
+      end
+
+      context "with an object" do
+        it "removes a previously added stub" do
+          registry = create_registry
+          object = 42
+
+          stub = create_example_stub
+          call = create_example_call
+
+          registry.add_stub(object, stub)
+          registry.clear(object)
+          registry.find_stub(object, call).should be_nil
+        end
+
+        it "removes a previously added call" do
+          registry = create_registry
+          object = 42
+          call = create_example_call
+
+          registry.add_call(object, call)
+          registry.clear(object)
+          registry.calls(object).empty?.should be_true
+        end
+
+        it "doesn't modify other object stubs" do
+          registry = create_registry
+          object1 = 42
+          object2 = :xyz
+
+          stub1 = create_example_stub
+          stub2 = create_example_stub
+          call = create_example_call
+
+          registry.add_stub(object1, stub1)
+          registry.add_stub(object2, stub2)
+          registry.clear(object1)
+          registry.find_stub(object2, call).should be(stub2)
+        end
+
+        it "doesn't modify other object calls" do
+          registry = create_registry
+          object1 = 42
+          object2 = :xyz
+
+          call1 = create_example_call
+          call2 = create_example_call
+
+          registry.add_call(object1, call1)
+          registry.add_call(object2, call2)
+          registry.clear(object1)
+          registry.calls(object2).should contain(call2)
+        end
+
+        it "doesn't confuse two objects with differing types and the same bytes" do
+          registry = create_registry
+          object1 = ExampleRecord1.new(42)
+          object2 = ExampleRecord2.new(42)
+
+          stub1 = create_example_stub
+          stub2 = create_example_stub
+          call1 = create_example_call
+          call2 = create_example_call
+
+          registry.add_stub(object1, stub1)
+          registry.add_stub(object2, stub2)
+          registry.add_call(object1, call1)
+          registry.add_call(object2, call2)
+
+          registry.clear(object1)
+          registry.find_stub(object1, call1).should be_nil
+          registry.find_stub(object2, call2).should be(stub2)
+          registry.calls(object1).empty?.should be_true
+          registry.calls(object2).should contain(call2)
+        end
+      end
+    end
+  end
+end
--- src/spectator/mocks/stub_registry.cr
+++ src/spectator/mocks/stub_registry.cr
@@ -4,8 +4,22 @@ require "./stub"
 
 module Spectator::Mocks
   # Stores and retrieves method stubs and calls for multiple objects.
-  # The type parameter *T* indicates the type(s) that can be stored.
-  abstract class StubRegistry(T)
+  #
+  # Reference-based objects are tracked by their `#object_id` (memory address).
+  # Value-based objects are tracked by their byte-wise representation.
+  #
+  # NOTE: This approach does not work for mutable values, and their use is generally discouraged.
+  #   This also implies that two separate values with the same content will be tracked as the same object.
+  #
+  # See: https://crystal-lang.org/reference/1.7/syntax_and_semantics/structs.html
+  class StubRegistry
+    # Entry keys consist of two elements - the type's name and it's location in memory.
+    # For value-types, it's byte-representation is is stored in a pool and that location is used.
+    #
+    # NOTE: Ideally, the type's ID would be used as the first element of the tuple.
+    #   `Object.class` cannot be used due to https://github.com/crystal-lang/crystal/issues/9667
+    alias Key = {String, UInt64}
+
     # Structure used to store stubs and calls for an object.
     private struct Entry
       # Stubs added to an object.
@@ -15,16 +29,25 @@ module Spectator::Mocks
       getter calls = [] of Call
     end
 
+    # Entries are stored and lazily created with a hash.
+    @entries = Hash(Key, Entry).new do |hash, key|
+      hash[key] = Entry.new
+    end
+
+    # String pool used to de-duplicate and track values.
+    @pool = StringPool.new
+
     # Adds a stub for an object.
-    def add_stub(object : T, stub : Stub) : Nil
-      entry = fetch(object)
-      entry.stubs << stub
+    def add_stub(object, stub : Stub) : Nil
+      key = generate_key(object)
+      @entries[key].stubs << stub
     end
 
     # Finds a stub suitable stub for a method call.
     # Returns nil if no stub was found.
-    def find_stub(object : T, call : Call) : Stub?
-      return unless entry = fetch?(object)
+    def find_stub(object, call : Call) : Stub?
+      key = generate_key(object)
+      return unless entry = @entries[key]?
 
       entry.stubs.find do |stub|
         stub === call
@@ -32,21 +55,21 @@ module Spectator::Mocks
     end
 
     # Removes all previously stubs added for an object.
-    def clear_stubs(object : T) : Nil
-      return unless entry = fetch?(object)
-
-      entry.stubs.clear
+    def clear_stubs(object) : Nil
+      key = generate_key(object)
+      @entries[key]?.try &.stubs.clear
     end
 
     # Records a method call made to an object.
-    def add_call(object : T, call : Call) : Nil
-      entry = fetch(object)
-      entry.calls << call
+    def add_call(object, call : Call) : Nil
+      key = generate_key(object)
+      @entries[key].calls << call
     end
 
     # Retrieves all method calls made to an object.
-    def calls(object : T) : Enumerable(Call)
-      if entry = fetch?(object)
+    def calls(object) : Enumerable(Call)
+      key = generate_key(object)
+      if entry = @entries[key]?
         entry.calls
       else
         [] of Call
@@ -54,24 +77,50 @@ module Spectator::Mocks
     end
 
     # Removes all previously recorded method calls for an object.
-    def clear_calls(object : T) : Nil
-      return unless entry = fetch?(object)
-
-      entry.calls.clear
+    def clear_calls(object) : Nil
+      key = generate_key(object)
+      @entries[key]?.try &.calls.clear
     end
 
     # Removes all stored stubs and calls for all objects.
-    abstract def clear : Nil
+    def clear : Nil
+      @entries.clear
+      # NOTE: There's no method for clearing a `StringPool`.
+      #   Recreate it to allow GC to cleanup.
+      @pool = StringPool.new
+    end
 
     # Removes all stored stubs and calls for the specified object.
-    abstract def clear(object : T) : Nil
+    def clear(object) : Nil
+      key = generate_key(object)
+      @entries.delete(key)
+      # NOTE: There's no method for deleting an item from a `StringPool`.
+    end
 
-    # Attempts to retrieve an entry for the specified object.
-    # Nil is returned if the object isn't tracked or its data has been deleted.
-    private abstract def fetch?(object : T) : Entry?
+    # Generates the hash key used for the specified object.
+    # This uses the object's type name and its ID.
+    private def generate_key(object : Reference) : Key
+      {object.class.name, object.object_id}
+    end
 
-    # Retrieves an entry for the specified object.
-    # An entry is created for the object if it doesn't exist.
-    private abstract def fetch(object : T) : Entry
+    # Generates the hash key used for the specified object.
+    # This computes the compound key consisting of the type's name and the value's byte representation.
+    #
+    # The object's value is copied from the stack (this method's scope) to the heap.
+    # A string pool is utilized to de-duplicate values and reduce memory consumption.
+    # It also acts as the mechanism to copy the value to the heap and get a suitable object ID (memory address).
+    private def generate_key(object : Value) : Key
+      # Get a slice referring to the contents of the object.
+      # This is only valid for the duration of this method's scope.
+      ptr = pointerof(object).as(UInt8*)
+      size = sizeof(typeof(object))
+      bytes = Bytes.new(ptr, size, read_only: true)
+
+      # De-duplicate the value.
+      # This may allocate memory, but the value's bytes will be safely stored in the string pool.
+      string = @pool.get(bytes)
+
+      {object.class.name, string.object_id}
+    end
   end
 end
