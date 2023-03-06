4296c478fc37f7504ddee520bf44fc6d64e00d84
Michael Miller <icy.arctic.fox@gmail.com>
2023-03-05 18:49:05 -0700

    Support specifying default stubs with keyword arguments

--- src/spectator/mocks/double.cr
+++ src/spectator/mocks/double.cr
@@ -1,4 +1,6 @@
+require "./stub"
 require "./stubbable"
+require "./value_stub"
 
 module Spectator::Mocks
   abstract class Double
@@ -32,8 +34,24 @@ module Spectator::Mocks
       end
     end
 
+    def initialize(name = nil, **value_stubs)
+      @name = name.try &.to_s
+      proxy = __mocks
+      value_stubs.each do |method_name, value|
+        stub = ValueStub.new(method_name, value)
+        proxy.add_stub(stub)
+      end
+    end
+
     def to_s(io : IO) : Nil
-      raise NotImplementedError.new("Double#to_s")
+      io << "#<" << self.class << ":0x"
+      object_id.to_s(io, 16)
+      if name = @name
+        io << "\"#{name}\""
+      else
+        io << "anonymous"
+      end
+      io << '>'
     end
   end
 end
