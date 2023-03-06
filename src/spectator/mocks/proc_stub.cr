c2e37c2ebbe73930a6cf94e9bfcc4d8b80e8446d
Michael Miller <icy.arctic.fox@gmail.com>
2023-03-05 17:26:48 -0700

    Add ProcStub

---
+++ src/spectator/mocks/proc_stub.cr
@@ -0,0 +1,26 @@
+require "./stub"
+
+module Spectator::Mocks
+  # Stub that calls a proc and returns its return value.
+  class ProcStub(T) < Stub
+    def initialize(method_name : Symbol, @proc : -> T, arguments : AbstractArgumentsPattern? = nil)
+      super(method_name, arguments)
+    end
+
+    def initialize(method_name : Symbol, arguments : AbstractArgumentsPattern? = nil, &@proc : -> T)
+      super(method_name, arguments)
+    end
+
+    def call(args : Arguments, & : -> U) : U forall U
+      {% if T <= U %}
+        @proc.call
+      {% else %}
+        raise TypeCastError.new("Attempted to return a #{T} from stub, but method `#{method_name}` expects type #{U}")
+      {% end %}
+    end
+
+    private def with_arguments(arguments : AbstractArgumentsPattern?)
+      {{@type.name(generic_args: false)}}.new(method_name, @proc, arguments)
+    end
+  end
+end
