3701125b464308793017f0168abd8067862eb91e
Michael Miller <icy.arctic.fox@gmail.com>
2023-03-11 17:49:35 -0700

    Move logic to stub an entire type to Stubbable
    Add support for filtering methods by name.
    Redefine ancestor methods matching the specified name.

--- src/spectator/mocks/stubbable.cr
+++ src/spectator/mocks/stubbable.cr
@@ -87,9 +87,88 @@ module Spectator::Mocks
 
       {% else %}
         # Stub all methods matching the specified name.
-        # TODO: Handle ancestors.
-        {% for m in @type.methods %}
-          {% if m.name == method.id %}stub {{m}}{% end %}
+        stub_existing_methods {{method}}
+      {% end %}
+    end
+
+    # Redefines all methods in the current type to support stubs.
+    # All methods in the current type and its ancestors are redefined.
+    # Only methods with a specific name can be redefined by providing a *name*.
+    private macro stub_existing_methods(name = nil)
+      {% definitions = [] of {Def, Symbol, Symbol?}
+         # Find all methods to redefine.
+         # Definitions are stored as an array of tuples.
+         # The first element is the method definition.
+         # The next two are the behavior (previous_def or super) and the method receiver (self or nil).
+
+         # Add methods from the current type.
+         # This must be done first so that the previous definition is used instead of one from an ancestor.
+         definitions = @type.methods.map do |method|
+           {method, :previous_def, nil}
+         end
+
+         # Add class methods from the current type.
+         definitions += @type.class.methods.map do |method|
+           {method, :previous_def, :self}
+         end
+
+         # Add methods from ancestors if they aren't overridden.
+         @type.ancestors.each do |ancestor|
+           # DRY up code by combining instance and class methods from ancestor.
+           variants = [
+             {ancestor, nil},
+             {ancestor.class, ancestor.name(generic_args: false)},
+           ]
+           variants.each do |(type, receiver)|
+             type.methods.each do |method|
+               # Skip methods overridden by a sub-type to prevent unnecessary redefinitions.
+               unless definitions.any? do |(m, _, _)|
+                        # Method objects can't be directly compared.
+                        # Compare each distinguishing attribute separately.
+                        m.name == method.name &&
+                        m.args == method.args &&
+                        m.splat_index == method.splat_index &&
+                        m.double_splat == method.double_splat &&
+                        m.block_arg == method.block_arg
+                      end
+                 # Method not overridden, add it to the list.
+                 definitions << {method, :super, receiver}
+               end
+             end
+           end
+         end
+
+         # Filter out methods that should be skipped, are incompatible, or unsafe to stub.
+         definitions = definitions.reject do |(method, _, _)|
+           ::Spectator::Mocks::Stubbable::Automatic::SKIPPED_METHOD_NAMES.includes?(method.name.symbolize) ||
+             method.name.starts_with?("__") ||
+             method.annotation(Primitive) || method.annotation(::Spectator::Mocks::Stubbed)
+         end
+
+         if name
+           # Only redefine methods with the specified name.
+           name = name.id
+           definitions = definitions.select do |(method, _, _)|
+             method.name == name
+           end
+         end %}
+
+      # Redefine virtually all methods to support stubs.
+      # FIXME: Reuse method signature generation code.
+      {% for definition in definitions %}
+        {% method, behavior, receiver = definition
+           visibility = method.visibility %}
+        {% begin %}
+          @[::Spectator::Mocks::Stubbed]
+          {{visibility.id if visibility != :public}} def {{"self.".id if receiver}}{{method.name}}{% unless method.args.empty? %}({% for arg, i in method.args %}
+            {% if i == method.splat_index %}*{% end %}{{arg}}, {% end %}{% if method.double_splat %}**{{method.double_splat}}, {% end %}
+            {% if method.block_arg %}&{{method.block_arg}}{% elsif method.accepts_block? %}&{% end %}
+          ){% end %}{% if method.return_type %} : {{method.return_type}}{% end %}{% unless method.free_vars.empty? %} forall {{*method.free_vars}}{% end %}
+            # For abstract methods, if no return type is specified, it will be `NoReturn`.
+            # It is expected that the method is overridden if another type is needed.
+            # Requiring a return type is not allowed here since it could require changes outside the user's code.
+            stubbed_method_body({{behavior}})
+          end
         {% end %}
       {% end %}
     end
--- src/spectator/mocks/stubbable/automatic.cr
+++ src/spectator/mocks/stubbable/automatic.cr
@@ -19,71 +19,7 @@ module Spectator::Mocks
       # Add stub support to class methods.
       extend ::Spectator::Mocks::Stubbable
 
-      {% definitions = [] of {Def, Symbol, Symbol?}
-         # Find all methods to redefine.
-         # Definitions are stored as an array of tuples.
-         # The first element is the method definition.
-         # The next two are the behavior (previous_def or super) and the method receiver (self or nil).
-
-         # Add methods from the current type.
-         # This must be done first so that the previous definition is used instead of one from an ancestor.
-         definitions = @type.methods.map do |method|
-           {method, :previous_def, nil}
-         end
-
-         # Add class methods from the current type.
-         definitions += @type.class.methods.map do |method|
-           {method, :previous_def, :self}
-         end
-
-         # Add methods from ancestors if they aren't overridden.
-         @type.ancestors.each do |ancestor|
-           # DRY up code by combining instance and class methods from ancestor.
-           variants = [
-             {ancestor, nil},
-             {ancestor.class, ancestor.name(generic_args: false)},
-           ]
-           variants.each do |(type, receiver)|
-             type.methods.each do |method|
-               # Skip methods overridden by a sub-type to prevent unnecessary redefinitions.
-               unless definitions.any? do |(m, _, _)|
-                        # Method objects can't be directly compared.
-                        # Compare each distinguishing attribute separately.
-                        m.name == method.name &&
-                        m.args == method.args &&
-                        m.splat_index == method.splat_index &&
-                        m.double_splat == method.double_splat &&
-                        m.block_arg == method.block_arg
-                      end
-                 # Method not overridden, add it to the list.
-                 definitions << {method, :super, receiver}
-               end
-             end
-           end
-         end
-
-         # Filter out methods that should be skipped, are incompatible, or unsafe to stub.
-         definitions = definitions.reject do |(method, _, _)|
-           ::Spectator::Mocks::Stubbable::Automatic::SKIPPED_METHOD_NAMES.includes?(method.name.symbolize) ||
-             method.name.starts_with?("__") ||
-             method.annotation(Primitive) || method.annotation(::Spectator::Mocks::Stubbed)
-         end %}
-
-      # Redefine virtually all methods to support stubs.
-      # FIXME: Reuse method signature generation code.
-      {% for definition in definitions %}
-        {% method, behavior, receiver = definition
-           visibility = method.visibility %}
-        {% begin %}
-          @[::Spectator::Mocks::Stubbed]
-          {{visibility.id if visibility != :public}} def {{"self.".id if receiver}}{{method.name}}{% unless method.args.empty? %}({% for arg, i in method.args %}
-            {% if i == method.splat_index %}*{% end %}{{arg}}, {% end %}{% if method.double_splat %}**{{method.double_splat}}, {% end %}
-            {% if method.block_arg %}&{{method.block_arg}}{% elsif method.accepts_block? %}&{% end %}
-          ){% end %}{% if method.return_type %} : {{method.return_type}}{% end %}{% unless method.free_vars.empty? %} forall {{*method.free_vars}}{% end %}
-            stubbed_method_body({{behavior}})
-          end
-        {% end %}
-      {% end %}
+      stub_existing_methods
 
       # Automatically apply to all sub-types.
       {% if @type.module? %}
