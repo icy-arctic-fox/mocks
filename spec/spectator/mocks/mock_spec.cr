46cf1a4453edc751d04332bc16ff4cc758f6085b
Michael Miller <icy.arctic.fox@gmail.com>
2023-08-23 15:08:22 -0600

    Move remaining variants to their own spec files

--- spec/spectator/mocks/mock_spec.cr
+++ spec/spectator/mocks/mock_spec.cr
@@ -2,36 +2,5 @@ require "../../spec_helper"
 
 private alias Mock = Spectator::Mocks::Mock
 
-private abstract struct AbstractStruct
-end
-
-private Mock.define AbstractStructMock < AbstractStruct
-
-private module ModuleMixin
-end
-
-private Mock.define ModuleMixinMock < ModuleMixin
-
 describe Mock do
-  describe ".define" do
-    context "with an abstract struct" do
-      it "defines a sub-type" do
-        AbstractStructMock.should be < AbstractStruct
-      end
-
-      it "is instantiable" do
-        AbstractStructMock.new.should be_a(AbstractStruct)
-      end
-    end
-
-    context "with a module mixin" do
-      it "defines a sub-type" do
-        ModuleMixinMock.should be < ModuleMixin
-      end
-
-      it "is instantiable" do
-        ModuleMixinMock.new.should be_a(ModuleMixin)
-      end
-    end
-  end
 end
