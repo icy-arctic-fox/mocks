e87596a232098a98fd7c622c98066a825248146b
Michael Miller <icy.arctic.fox@gmail.com>
2023-08-25 19:41:54 -0600

    Remove tests around stubbed module class methods
    This is currently not supported.
    Removing these tests fixes the compilation error.

--- spec/spectator/mocks/mock_mixin_module_spec.cr
+++ spec/spectator/mocks/mock_mixin_module_spec.cr
@@ -7,10 +7,11 @@ private module MixinModule
   def_class_methods
 end
 
-private define_mock(MixinModuleMock < MixinModule, [:abstract, :concrete], [:abstract, :concrete, :class])
+private define_mock(MixinModuleMock < MixinModule, [:abstract, :concrete], [:abstract, :concrete])
 
 describe Spectator::Mocks::Mock do
   it_supports_abstract_methods(MixinModuleMock.new)
   it_supports_concrete_methods(MixinModuleMock.new)
-  it_supports_class_methods(MixinModuleMock)
+  # Class methods on modules are not supported (yet).
+  # it_supports_class_methods(MixinModuleMock)
 end
