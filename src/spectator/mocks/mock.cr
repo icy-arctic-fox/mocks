942d06ffd60ab168bc681f25b780bf32a8d162d5
Michael Miller <icy.arctic.fox@gmail.com>
2023-09-05 23:34:12 -0600

    Whitespace

--- src/spectator/mocks/mock.cr
+++ src/spectator/mocks/mock.cr
@@ -28,20 +28,20 @@ module Spectator::Mocks
                               else
                                 raise "Unsupported mock type for #{parent.name}"
                               end %}
-    
+
             {% begin %}
               {% if type_keyword == :module %}
                 module {{type}}
                   include {{parent}}
-    
+
                   class Instance
                     include {{type}}
-    
+
                     # Empty initializer to override the `.new` method from {{type}}.
                     def initialize
                     end
                   end
-    
+
                   @[::Spectator::Mocks::Stubbed]
                   def self.new : Instance
                     Instance.new
@@ -50,15 +50,15 @@ module Spectator::Mocks
                 {{type_keyword.id}} {{type}} < {{parent.name}}
               {% end %}
                 include ::Spectator::Mocks::Stubbable::Automatic
-    
+
                 {% for name, value in stubs %}
                   stub_existing({{name}}) { {{value}} }
                 {% end %}
-    
+
                 {{block.body if block}}
               end
             {% end %}
-    
+
           {% else %}
             {% raise "Syntax error in mock definition. Must be: `NewType < OriginalType`" %}
           {% end %}
