469286af6eaaa93e2c521568ac4982015cc6ca48
Michael Miller <icy.arctic.fox@gmail.com>
2023-09-06 15:44:34 -0600

    More mock test cases

---
+++ spec/spectator/mocks/mock_included_module_spec.cr
@@ -0,0 +1,34 @@
+require "../../spec_helper"
+require "./mock_spec"
+
+private module MixinModule
+  def_abstract_instance_methods
+  def_concrete_instance_methods
+end
+
+private abstract class MixedClass
+  include MixinModule
+end
+
+private abstract struct MixedStruct
+  include MixinModule
+end
+
+private module MixedModule
+  include MixinModule
+end
+
+private define_mock(MixedClassMock < MixedClass, [:abstract, :concrete], [:abstract, :concrete])
+private define_mock(MixedStructMock < MixedStruct, [:abstract, :concrete], [:abstract, :concrete])
+private define_mock(MixedModuleMock < MixedModule, [:abstract, :concrete], [:abstract, :concrete])
+
+describe Spectator::Mocks::Mock do
+  it_supports_abstract_methods(MixedClassMock.new)
+  it_supports_concrete_methods(MixedClassMock.new)
+
+  it_supports_abstract_methods(MixedStructMock.new)
+  it_supports_concrete_methods(MixedStructMock.new)
+
+  it_supports_abstract_methods(MixedModuleMock.new)
+  it_supports_concrete_methods(MixedModuleMock.new)
+end
---
+++ spec/spectator/mocks/mock_inherited_spec.cr
@@ -0,0 +1,31 @@
+require "../../spec_helper"
+require "./mock_spec"
+
+private abstract class BaseClass
+  def_abstract_instance_methods
+  def_concrete_instance_methods
+  def_class_methods
+end
+
+private abstract struct BaseStruct
+  def_abstract_instance_methods
+  def_concrete_instance_methods
+  def_class_methods
+end
+
+private abstract class ChildClass < BaseClass; end
+
+private abstract struct ChildStruct < BaseStruct; end
+
+private define_mock(ChildClassMock < ChildClass, [:abstract, :concrete], [:abstract, :concrete, :class])
+private define_mock(ChildStructMock < ChildStruct, [:abstract, :concrete], [:abstract, :concrete, :class])
+
+describe Spectator::Mocks::Mock do
+  it_supports_abstract_methods(ChildClassMock.new)
+  it_supports_concrete_methods(ChildClassMock.new)
+  it_supports_class_methods(ChildClassMock)
+
+  it_supports_abstract_methods(ChildStructMock.new)
+  it_supports_concrete_methods(ChildStructMock.new)
+  it_supports_class_methods(ChildStructMock)
+end
