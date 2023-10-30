30bca4811b1d307a02de86af01a049f1ca1b3aad
Michael Miller <icy.arctic.fox@gmail.com>
2023-10-30 16:07:58 -0600

    Add push and pop

--- src/spectator/mocks/scope.cr
+++ src/spectator/mocks/scope.cr
@@ -11,6 +11,32 @@ module Spectator::Mocks
       @@stack.last
     end
 
+    # Starts a new scope.
+    # `#pop` should be called after the scope is no longer needed.
+    def self.push : self
+      scope = new
+      @@stack << scope
+      scope
+    end
+
+    # Starts a new scope for the duration of the block.
+    # Yields the new scope to the block and returns the value from the block.
+    # `#pop` is called automatically as part of this method.
+    def self.push(& : self -> _)
+      push
+      yield current
+    ensure
+      pop
+    end
+
+    # Ends a previous scope.
+    # This should be called for each `#push` without a block.
+    def self.pop : Nil
+      raise "Cannot pop from root scope" if @@stack.size <= 1
+
+      @@stack.pop
+    end
+
     # Registry for the scope.
     getter registry : Registry = Registry.new
   end
