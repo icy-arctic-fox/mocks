09a5b085d3529946e90c7bd64dd4f0b39ae080fb
Michael Miller <icy.arctic.fox@gmail.com>
2024-05-07 18:13:34 -0600

    Additional information for auto-added stubs

--- src/mocks/stubbable/automatic.cr
+++ src/mocks/stubbable/automatic.cr
@@ -57,7 +57,10 @@ module Mocks
                 # For abstract methods, if no return type is specified, it will be `NoReturn`.
                 # It is expected that the method is overridden if something else is needed.
                 # Requiring a return type is not allowed here since it could require changes outside the user's code.
-                stubbed_method_body({{method.abstract? ? :unexpected : :previous_def}})
+                stubbed_method_body({{method.abstract? ? :abstract : :previous_def}},
+                  as: {{method.return_type || :infer}},
+                  {% if method.block_arg && method.block_arg.name && method.block_arg.name.size > 0 %}captured_block_name: {{method.block_arg.name}}{% end %}
+                )
               end
             {% end %}
           {% end %}
@@ -69,7 +72,10 @@ module Mocks
               {% if i == method.splat_index %}*{% end %}{{arg}}, {% end %}{% if method.double_splat %}**{{method.double_splat}}, {% end %}
               {% if method.block_arg %}&{{method.block_arg}}{% elsif method.accepts_block? %}&{% end %}
             ){% if method.return_type %} : {{method.return_type}}{% end %}{% unless method.free_vars.empty? %} forall {{method.free_vars.splat}}{% end %}
-              stubbed_method_body(:previous_def)
+              stubbed_method_body(:previous_def,
+                as: {{method.return_type || :infer}},
+                {% if method.block_arg && method.block_arg.name && method.block_arg.name.size > 0 %}captured_block_name: {{method.block_arg.name}}{% end %}
+              )
             end
           {% end %}
         end
